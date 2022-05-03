2711b27135d358ecc32498dc89c8f11a90bcdae8
Gustavo da Silva de Souza <gusssouza23@gmail.com>
2022-05-02 22:14:25 -0300

    atividades aula 5/15 lógica programação

---
+++ aula5.js
@@ -0,0 +1,44 @@
+a = Math.random().toFixed(0) * 10
+b = Math.random().toFixed(0) * 10
+c = Math.random().toFixed(0) * 10
+
+
+
+
+//if (a == 0) {
+//    alert("Não é função de 2 grau")
+//} else alert( a + ' ')
+
+//d = Math.random()
+// continuar em casa
+
+
+
+
+
+// atividade 1
+// Cadastrar e mostrar no video do computador os dados de um professor: nome, endereço, cidade, uf, cep, telefone, cpf, rg, data de nascimento, grau de escolaridade, curso que leciona.
+
+
+// atividade 2
+// Cadastrar e imprimir os dados de uma disciplina escolar: nome, descrição, frequência e média mínima para aprovação.
+
+
+
+
+
+
+//function Mostrar() {
+var nome = document.getElementById("Nome").value;
+var endereco = document.getElementById("Endereco").value;
+var cidade = document.getElementById("Cidade").value;
+var UF = document.getElementById("UF").value;
+var CEP = document.getElementById("CEP").value;
+var telefone = document.getElementById("telefone").value;
+var CPF = document.getElementById("cpf").value;
+var RG = document.getElementById("RG").value;
+var Data = document.getElementById("dataNascimento").value;
+var curso = document.getElementById("curso").value;
+
+//alert(nome + " " + endereco + " " + cidade + " " + UF + " " + CEP + " " + telefone + " " + CPF + " " + RG + " " + Data + " " + " " + curso)
+//}
---
+++ hamburgueria.js
@@ -0,0 +1,4 @@
+function pedido(){
+var cliente = document.getElementById("input").value
+if (cliente == 1) {alert("BIG SUPER SANDUBA   2 hamburgueres, queijo, batata frita e refrigerante  VALOR: R$5,00")} 
+else if (cliente == 2) {alert("QUASE SUPER SANDUBA  R$3,00")} else if (cliente == 3){alert("MIRRADUS SANDUBA  1,50")}}
